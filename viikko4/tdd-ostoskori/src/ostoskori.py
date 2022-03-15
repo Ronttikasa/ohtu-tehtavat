8f2ca893c793fd3561137048449ad939a3683537
Silja <44512829+Ronttikasa@users.noreply.github.com>
2022-03-15 18:02:48 +0200

    yhden tuotteen lisäämisen jälkeen ostokset-metodi palauttaa oikean mittaisen listan

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -3,23 +3,23 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        self.ostokset = []
+        self.sisalto = []
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return len(self.ostokset)
+        return len(self.sisalto)
 
     def hinta(self):
         hinta = 0
-        if not len(self.ostokset) == 0:
-            for ostos in self.ostokset:
+        if not len(self.sisalto) == 0:
+            for ostos in self.sisalto:
                 hinta += ostos.hinta()
         return hinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
         uusi_ostos = Ostos(lisattava)
-        self.ostokset.append(uusi_ostos)
+        self.sisalto.append(uusi_ostos)
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
@@ -30,6 +30,6 @@ class Ostoskori:
         # tyhjentää ostoskorin
 
     def ostokset(self):
-        pass
+        return self.sisalto
         # palauttaa listan jossa on korissa olevat ostos-oliot
         # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
